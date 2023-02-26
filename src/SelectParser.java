e818a8e8049c46e38a9ad829875366395f17b140
bakerh22 <123035980+bakerh22@users.noreply.github.com>
2023-02-25 22:10:47 -0800

    Create SelectParser.java
    Parses the select command and creates a new Relation with only the tuples specified in the where clause

---
+++ src/SelectParser.java
@@ -0,0 +1,41 @@
+public class SelectParser {
+    private String input;
+
+    SelectParser(String input) {
+        this.input=input.split(";")[0];
+    }
+    public String parseRelationName() {
+        return input.split("\\s+")[1];
+    }
+
+    //method that takes a relation and returns a temporary relation with the tuples
+    //pruned. T1 = SELECT OFFERING WHERE CNUM = CSCI241 and SECTION > 27922;
+    public Relation selectWhere(Relation relation, String name) {
+        WhereParser whereParser = new WhereParser(getWhereClause());
+        Relation tempRelation = copyRelation(relation, name);
+        tempRelation.delete();
+        tempRelation.setName(name);
+        int relationSize = relation.size();
+        for (int i = 0; i<relationSize; i++) {
+            Tuple temp = relation.getTuple(i);
+            if (whereParser.meetsConditions(temp)) {
+                tempRelation.insert(temp);
+            }
+        }
+        return tempRelation;
+    }
+
+    private Relation copyRelation(Relation relation, String name) {
+        Relation newRelation = new Relation(name);
+        int schemaSize = relation.schemaSize();
+        for (int i = 0; i<schemaSize; i++) {
+            newRelation.addToSchema(relation.getSchema(i));
+        }
+        return newRelation;
+    }
+
+    private String getWhereClause() {
+        int whereIndex = input.toUpperCase().indexOf("WHERE");
+        return input.substring(whereIndex, input.length());
+    }
+}
