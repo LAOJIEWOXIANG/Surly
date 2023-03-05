19f6c0ef6d2132af7a1434fca7a525fe067d65c8
Preston-D <prestonduffield1@gmail.com>
2023-03-04 16:23:00 -0800

    chore: code cleanup

--- src/JoinParser.java
+++ src/JoinParser.java
@@ -7,8 +7,9 @@ public class JoinParser {
 
   /* Parses and returns the names the relations to join. */
   public String[] parseRelationNames() {
-    // Splits the string on JOIN, then on ON and creates a list of the relations inbetween the two.
-    String[] output = this.input.split("JOIN ")[1].split(" ON ")[0].split(", ");    
+    // Splits the string on JOIN, then on ON and creates a list of the relations
+    // inbetween the two.
+    String[] output = this.input.split("JOIN ")[1].split(" ON ")[0].split(", ");
     return output;
   }
 
@@ -17,12 +18,12 @@ public class JoinParser {
     // Split the input on "JOIN " to isolate the join condition
     String[] intermediate = input.split("JOIN ");
     if (intermediate.length > 1) {
-        // Split the join condition on " ON " to isolate the comparison expression
-        String[] comparison = intermediate[1].split(" ON ");
-        if (comparison.length > 1) {
-            // Store the comparison expression in the output array
-            outputArray[0] = comparison[1];
-        }
+      // Split the join condition on " ON " to isolate the comparison expression
+      String[] comparison = intermediate[1].split(" ON ");
+      if (comparison.length > 1) {
+        // Store the comparison expression in the output array
+        outputArray[0] = comparison[1];
+      }
     }
     return outputArray;
   }
