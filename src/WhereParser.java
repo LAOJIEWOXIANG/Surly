7efafa64b15432a9f7af5c06b32363bc5766ea7c
Your Name Goes Here <zhengy@wwu.edu>
2023-03-05 22:33:31 -0800

    update WhereParser

--- src/WhereParser.java
+++ src/WhereParser.java
@@ -50,16 +50,16 @@ public class WhereParser {
         boolean second = first;
         /* Evaluates the expression from left to right, grouping all the "and"s together */
         for (int i = 0; i<ops; i++) {
-            if (logicalOperators.get(i).equalsIgnoreCase("or")) {
+            if (logicalOperators.get(i).equals("or")) {
                 second = evaluate(tuple,i+1); 
-                while (i+1 < ops && logicalOperators.get(i+1).equalsIgnoreCase("and")) {
+                while (i+1 < ops && logicalOperators.get(i+1).equals("and")) {
                         i++;
                         second = second && evaluate(tuple,i+1);
                 }
                 first = first || second;
             } else {
                 first = first && evaluate(tuple,i+1);
-                while (i+1 < ops && logicalOperators.get(i+1).equalsIgnoreCase("and")) {
+                while (i+1 < ops && logicalOperators.get(i+1).equals("and")) {
                     i++;
                     first = first && evaluate(tuple,i+1);
                 }
